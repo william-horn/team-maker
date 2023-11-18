084305bced5ba475749b8b7f5cb85d1b1b8050ab
William Horn <williamjosephhorn@gmail.com>
2023-11-18 08:20:22 -0500

    initial icon testing

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,22 @@
 import Image from 'next/image'
-import Icon from '@/components/Images/Icon'
+import Icon from '@/components/Graphics/Icon'
 
 export default function Home() {
   return (
     <main>
       <p className="">Hello world</p>
+
+      {/* Testing utility icon responsiveness  */}
       <Icon
-      src="/images/bourbon-pecan.webp"
-      width="50vw"
+      src="/images/utility-icon-test.svg"
+      utility
+      self_className="w-[48px] h-[48px] sm:w-[96px] sm:h-[96px] lg:w-[192px] lg:h-[192px]"
+      />
+
+      {/* Testing decorative icon responsiveness  */}
+      <Icon
+      src="/images/decorative-icon-test.webp"
+      self_className="w-[128px] h-[128px] sm:w-[256px] sm:h-[256px] lg:w-[512px] lg:h-[512px]"
       />
     </main>
   )
---
+++ components/Graphics/Icon.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+
+export default function Icon({ 
+  src, 
+  width, 
+  height, 
+  alt, 
+  utility=false,
+  self_className=""
+}) {
+  height = height || width
+
+  /* 
+  TODO: 
+  
+  Increase utility icon resolution at different breakpoints. In the meantime just use 48px
+  as a constant. Ideally, new utility icon downloads should come in at the same time as the 
+  rendered icon breakpoint.
+
+  notes:
+  *All original utility icons should have an initial resolution of 192px
+  *All decorative icons should have an initial resolution of 512px
+
+  Utility icon breakpoints:
+    - sm: 48px 
+    - md: 96px
+    - lg: 192px
+
+  Decorative icon breakpoints:
+    - sm: 128px
+    - md: 256px
+    - lg: 512px
+
+  Create the icon image in the maximum resolution that can be used. The download size and
+  quality of the image will change based on breakpoints.
+  */
+
+  return (
+    <div 
+    className={"relative overflow-hidden icon " + self_className}
+    style={{ width, height }}
+    >
+      <Image
+      fill
+      src={src}
+      sizes={
+        utility ? 
+          "(min-width: 1024px) 192px, (min-width: 640px) 96px, 48px"
+          : "(min-width: 1024px) 512px, (min-width: 640px) 256px, 128px"
+      }
+      alt={alt || "icon alongside text"}
+      />
+    </div>
+  )
+}
