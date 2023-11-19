887bf5d46eb7a02bffc0f2fb099c4722fa9b6b30
William Horn <williamjosephhorn@gmail.com>
2023-11-19 18:42:26 -0500

    working multi-button selection group

--- components/Buttons/ButtonGroup.jsx
+++ components/Buttons/ButtonGroup.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useRef } from "react";
 import ButtonGroupProvider from "@/providers/ButtonGroupProvider";
 import { useButtonGroupContext } from "@/providers/ButtonGroupProvider";
 import Button from "./Button";
@@ -7,25 +8,66 @@ import emptyFunc from "@/util/emptyFunc";
 
 const GroupButton = ({ 
   children,
+
   onClick=emptyFunc,
-  option="default",
+  onSelect=emptyFunc,
+  onUnselect=emptyFunc,
+
+  id,
+  value,
+  
   ...rest
 }) => {
   const buttonGroupContext = useButtonGroupContext();
 
   const {
     groupName,
-    onClick: groupClick
+    onClick: group_onClick,
+    mode,
+    findActiveId,
+    updateActiveIds,
+    onSelect: group_onSelect,
+    onUnselect: group_onUnselect,
   } = buttonGroupContext;
 
-  const buttonData = {
-    groupName,
-    option,
+  if (mode !== "select") {
+    throw Error("ButtonGroup.Button components can only be used in <ButtonGroup> with mode='select'");
+  } else if (!id) {
+    throw Error("ButtonGroup.Button components must be given a unique id prop");
+  }
+
+  const initiallySelected = findActiveId(id).found;
+  
+  const buttonData = { 
+    id, 
+    value, 
+    groupName, 
+    isSelected: initiallySelected 
+  };
+
+  const fireOnSelect = (...args) => {
+    group_onSelect(...args);
+    onSelect(...args);
+  }
+
+  const fireOnUnselect = (...args) => {
+    group_onUnselect(...args);
+    onUnselect(...args);
   }
 
   const buttonClick = () => {
-    groupClick(buttonData);
+    buttonData.isSelected = !initiallySelected;
+    
+    group_onClick(buttonData);
     onClick(buttonData);
+
+    if (buttonData.isSelected) {
+      fireOnSelect(buttonData);
+    } else {
+      fireOnUnselect(buttonData);
+    }
+
+    updateActiveIds(id, buttonData.isSelected);
   }
 
   return (
@@ -40,12 +82,62 @@ const ButtonGroup = ({
 
   // handlers
   onClick=emptyFunc,
+  onSelect=emptyFunc,
+  onUnselect=emptyFunc,
 
+  defaultSelect=[],
   groupName="Button Group",
   mode="select",
 
   limit=false,
 }) => {
+  const multipleSelected = Array.isArray(defaultSelect);
+
+  const [activeIds, _setActiveIds] = useState(() => {
+    if (!multipleSelected) return [defaultSelect];
+    return defaultSelect;
+  });
+
+  const formData = useRef([]);
+
+  const findActiveId = (buttonId) => {
+    const idIndex = activeIds.findIndex(id => id === buttonId);
+    return { found: idIndex !== -1, index: idIndex };
+  }
+  
+  const updateActiveIds = (buttonId, isSelected) => {
+    if (isSelected) {
+      _setActiveIds(prev => {
+        if (multipleSelected) {
+
+          // ? checking to prevent duping button ids in react strict mode
+          if (!prev.some(id => id === buttonId)) {
+            prev.push(buttonId);
+            return [...prev];
+          }
+
+        } else {
+          prev[0] = buttonId;
+          return [...prev];
+        }
+
+        return prev;
+      });
+      
+    } else {
+      _setActiveIds(prev => {
+        const idResult = findActiveId(buttonId);
+
+        // ? checking to prevent duping button ids in react strict mode
+        if (idResult.found) {
+          prev.splice(idResult.index, 1);
+          return [...prev];
+        }
+
+        return prev;
+      });
+    }
+  }
 
   return (
     <ButtonGroupProvider
@@ -53,6 +145,10 @@ const ButtonGroup = ({
       onClick,
       groupName,
       mode,
+      findActiveId,
+      updateActiveIds,
+      onSelect,
+      onUnselect,
     }}
     >
       <div className="flex custom-button-group">
--- components/ClientComponent.jsx
+++ components/ClientComponent.jsx
@@ -16,27 +16,39 @@ const ClientComponent = ({ children }) => {
     console.log("button click: ", data);
   }
 
+  const buttonSelected = (data) => {
+    console.log("button has been selected: ", data);
+  }
+
+  const buttonUnselected = (data) => {
+    console.log("button has been unselected: ", data);
+  }
+
   return <div className="p-5">
 
     <ButtonGroup
     mode="select"
     limit="2"
+    defaultSelect="one"
     onClick={groupClick}
+    onSelect={buttonSelected}
+    onUnselect={buttonUnselected}
     groupName="group one"
     >
-      <ButtonGroup.Button option="one" onClick={buttonClick}>Choice One</ButtonGroup.Button>
-      <ButtonGroup.Button option="two">Choice Two</ButtonGroup.Button>
-      <ButtonGroup.Button option="three">Choice Three</ButtonGroup.Button>
+      <ButtonGroup.Button id="one">Choice One</ButtonGroup.Button>
+      <ButtonGroup.Button id="two">Choice Two</ButtonGroup.Button>
+      <ButtonGroup.Button id="three">Choice Three</ButtonGroup.Button>
     </ButtonGroup>
 
-    <ButtonGroup
+    {/* <ButtonGroup
     onClick={groupClick}
     groupName="group two"
+    mode="checkbox"
     >
-      <ButtonGroup.Button option="one" onClick={buttonClick}>Choice A</ButtonGroup.Button>
+      <ButtonGroup.Button option="one">Choice A</ButtonGroup.Button>
       <ButtonGroup.Button option="two">Choice B</ButtonGroup.Button>
       <ButtonGroup.Button option="three">Choice C</ButtonGroup.Button>
-    </ButtonGroup>
+    </ButtonGroup> */}
 
 
     {/* <ButtonGroup>
