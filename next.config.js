c4220fd19f98c30f18499f8940f10436a2f8c345
William Horn <williamjosephhorn@gmail.com>
2023-12-29 17:40:19 -0500

    disabled eslint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   eslint: {
-    // ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   },
 }
 
