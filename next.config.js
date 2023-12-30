1f0b919644657156b5e715479aef75235d0353b8
William Horn <williamjosephhorn@gmail.com>
2023-12-29 20:29:48 -0500

    adding env to config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint: {
     // ignoreDuringBuilds: true,
   },
+  env: {
+    MONGODB_URI: "mongodb+srv://williamjosephhorn:6TcYIe5j7KBcltJn@cluster0.kiyzjms.mongodb.net/"
+  }
 }
 
 module.exports = nextConfig
